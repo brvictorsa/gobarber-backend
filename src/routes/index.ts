2934d2959d4bab05738538166c1707b764ba3b52
Victor Santos <victorsa.mg@gmail.com>
2020-06-04 20:03:37 -0300

    Cadastro de agendamentos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import Router from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/users', (request, response) => {
-  const { name, email } = request.body;
-  response.json({ user: { name, email } });
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
