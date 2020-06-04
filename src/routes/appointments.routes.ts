46a93999a162f44c9e6042bb7b1f461183124438
Victor Santos <victorsa.mg@gmail.com>
2020-06-04 20:42:42 -0300

    Criação do repositório de agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,19 +1,18 @@
-import { Router, json } from 'express';
-import { uuid } from 'uuidv4';
-import { isEqual, parseISO, startOfHour } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { Router } from 'express';
+import { parseISO, startOfHour } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentIsSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentIsSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentIsSameDate) {
@@ -22,9 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
     });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
