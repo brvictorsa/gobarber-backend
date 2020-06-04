00106465112772ee5619f2cdebecad78a3c9feee
Victor Santos <victorsa.mg@gmail.com>
2020-06-04 20:18:06 -0300

    Validação de agendamento na mesma data/hora

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
-import { Router } from 'express';
+import { Router, json } from 'express';
 import { uuid } from 'uuidv4';
+import { isEqual, parseISO, startOfHour } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentIsSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentIsSameDate) {
+    return response.status(400).json({
+      message: 'There is already a scheduled appointment on this date',
+    });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
