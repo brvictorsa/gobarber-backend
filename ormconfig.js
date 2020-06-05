9401df8003ccd6529e9173e5a1e04312ff773e5f
Victor Santos <victorsa.mg@gmail.com>
2020-06-05 00:27:37 -0300

    Ciração da tabela de agendamentos (appointments)

--- ormconfig.js
+++ ormconfig.js
@@ -6,5 +6,9 @@ module.exports = {
   "port": process.env.DB_PORT,
   "database": process.env.DB_NAME,
   "username": process.env.DB_USERNAME,
-  "password": process.env.DB_PASSWORD
+  "password": process.env.DB_PASSWORD,
+  "migrations": ["./src/database/migrations/*.ts"],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
 }
